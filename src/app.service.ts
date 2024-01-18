640c5a09ff69d18176c979a182e61ac1d22ba327
JaviGP2002 <javigp2002@gmail.com>
2024-01-18 12:17:12 +0100

    :sparkles: Recoger datos db y mostrar

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import {DbConnection} from "./db/db.connection";
 
 @Injectable()
 export class AppService {
+  constructor(private dbConnection: DbConnection) {}
+
   getHello(): string {
     return 'Hello World!';
   }
+  async getUsers() {
+    let conn;
+    try {
+      conn = await this.dbConnection.createConnection();
+      // console.log(rows);
+      return await conn.query('SELECT * FROM users');
+    } catch (err) {
+      throw err;
+    } finally {
+      if (conn) conn.end();
+    }
+  }
+
 }
