6f7ff52f50df0db31145e19b0b6cdaa3245700da
JaviGP2002 <javigp2002@gmail.com>
2024-01-27 16:46:30 +0100

    :rocket: Add new  imports and providers a app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { UserModule } from './users/user.module';
 import { DbModule } from './db/db.module';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, DbModule],
+  imports: [UserModule, DbModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
