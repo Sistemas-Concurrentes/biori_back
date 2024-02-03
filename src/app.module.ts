1fe601c83d6566db79b211f54508a78422f6b221
JaviGP2002 <javigp2002@gmail.com>
2024-02-03 13:56:56 +0100

    :rocket: Add new controller to app
    Ahora ya podemos utilizar los entrypoints del modulo auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DbModule } from './datasource/db/db.module';
+import { AuthControllerModule } from './api/auth/controller/auth.controller.module';
 
 @Module({
-  imports: [DbModule],
+  imports: [DbModule, AuthControllerModule],
   controllers: [AppController],
   providers: [AppService],
 })
