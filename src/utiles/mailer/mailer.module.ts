7d9e84bd4021254fb95018f71fc04c7f41479ae1
JaviGP2002 <javigp2002@gmail.com>
2024-02-12 17:53:48 +0100

    :rocket: Add: Mailer which sends emails with code
    En este momento, podemos empezar a enviar emails que se reciban con el código de verificación

---
+++ src/utiles/mailer/mailer.module.ts
@@ -0,0 +1,36 @@
+import { Module } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+dotenv.config();
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { MyMailerService } from './mailer.service';
+import { MailerEntrypoint } from './mailer.entrypoint';
+
+@Module({
+  controllers: [MailerEntrypoint],
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        secure: false, // true for 465, false for other ports
+        auth: {
+          user: process.env.EMAIL_ID, // generated ethereal user
+          pass: process.env.EMAIL_PASS // generated ethereal password
+        },
+      },
+      defaults: {
+        from: `"No-Reply" ${process.env.EMAIL_ID}`,
+      },
+      template: {
+        dir: process.cwd() + '/src/utiles/mailer/template/',
+        adapter: new HandlebarsAdapter(), // or new PugAdapter()
+        options: {
+          strict: true,
+        },
+      },
+    }),
+  ],
+  providers: [MyMailerService],
+})
+export class MyMailerModule {}
