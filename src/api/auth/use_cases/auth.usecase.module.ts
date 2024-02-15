916bc069fd3e4c8c11c33cb149628103a011ce34
JaviGP2002 <javigp2002@gmail.com>
2024-02-15 11:54:48 +0100

    Add: validationUsecase al modulo de usecase

--- src/api/auth/use_cases/auth.usecase.module.ts
+++ src/api/auth/use_cases/auth.usecase.module.ts
@@ -3,12 +3,14 @@ import { LoginUsecase } from './login.usecase';
 import { UsertableModule } from '../../../datasource/db/usertable/usertable.module';
 import { MyJwtModule } from '../../../utiles/jwt/jwt.module';
 import { RegisterUsecase } from './register.usecase';
+import { ValidationUsecase } from './validation.usecase';
+import { MyMailerModule } from '../../../utiles/mailer/mailer.module';
 
 
 @Module({
   imports: [ UsertableModule, MyJwtModule],
   controllers: [],
-  providers: [LoginUsecase, RegisterUsecase],
-  exports: [LoginUsecase, RegisterUsecase],
+  providers: [LoginUsecase, RegisterUsecase, ValidationUsecase],
+  exports: [LoginUsecase, RegisterUsecase, ValidationUsecase],
 })
 export class AuthUsecaseModule {}
