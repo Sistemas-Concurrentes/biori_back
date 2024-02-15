7df2e657cabaa13e062ca61b8ca6f0432e69a60b
JaviGP2002 <javigp2002@gmail.com>
2024-02-15 11:58:22 +0100

    :rocket: ADd new entrypoint for validataion
    Ya podemos ejecutar la validación desde una petición a 'auth/validation'

--- src/api/auth/controller/auth.login.entrypoint.ts
+++ src/api/auth/controller/auth.login.entrypoint.ts
@@ -1,10 +1,12 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { SingInDto } from './dto/sing_in.dto';
 import { LoginUsecase } from '../use_cases/login.usecase';
+import { ValidationDto } from './dto/validation.dto';
+import { ValidationUsecase } from '../use_cases/validation.usecase';
 
 @Controller('auth')
 export class AuthLoginEntrypoint {
-  constructor (private loginUseCase: LoginUsecase) {}
+  constructor (private loginUseCase: LoginUsecase, private validateUseCase: ValidationUsecase) {}
 
   @HttpCode(HttpStatus.OK)
   @Post('login')
@@ -12,4 +14,9 @@ export class AuthLoginEntrypoint {
     return await this.loginUseCase.run(singInDto.user_name, singInDto.password);
   }
 
+  @HttpCode(HttpStatus.OK)
+  @Post('validation')
+  async validationCode (@Body() validationDto: ValidationDto){
+    return await this.validateUseCase.run(validationDto);
+  }
 }
