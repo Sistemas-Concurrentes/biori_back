fd1d980f8171e7eca99b2b385bcb27364a5822c1
JaviGP2002 <javigp2002@gmail.com>
2024-02-20 12:41:24 +0100

    :zap: Add Guard to auth controller
    Para ello, por la inyección de depednencias, tendremos q añadir también el MyJwtModule

--- src/api/auth/controller/auth.controller.module.ts
+++ src/api/auth/controller/auth.controller.module.ts
@@ -3,11 +3,13 @@ import { Module } from '@nestjs/common';
 import { AuthLoginEntrypoint } from './auth.login.entrypoint';
 import { AuthUsecaseModule } from '../use_cases/auth.usecase.module';
 import { AuthRegisterEntrypoint } from './auth.register.entrypoint';
+import { AuthGuardModule } from '../guards/auth.guard.module';
+import { MyJwtModule } from '../../../utiles/jwt/jwt.module';
 
 
 @Module({
-  imports: [ AuthUsecaseModule,],
-  controllers: [ AuthLoginEntrypoint, AuthRegisterEntrypoint ],
+  imports: [ AuthUsecaseModule, AuthGuardModule, MyJwtModule],
+  controllers: [ AuthLoginEntrypoint, AuthRegisterEntrypoint],
   providers: [ ],
   exports: [ ],
 })
