9c2ba15128c9659aede7916f692c67abb198261f
JaviGP2002 <javigp2002@gmail.com>
2024-03-26 17:49:55 +0100

    :sparkles: Add new module to release and get Reports
    Añadinmos la dependencia de los reports y los consultamos.

--- src/api/releases/use_cases/consult.usecase.ts
+++ src/api/releases/use_cases/consult.usecase.ts
@@ -2,17 +2,21 @@ import { Injectable } from '@nestjs/common';
 import { ConsultResult } from './dto/consult.result';
 import { Advertisementtable } from '../../../datasource/db/advertisementtable/advertisementtable.usecase';
 import { AdvertisementModel } from '../../../datasource/db/advertisementtable/model/advertisement.model';
+import { ReportModel } from '../../../datasource/db/reporttable/model/report.model';
+import { ReportTable } from '../../../datasource/db/reporttable/reporttable.usecase';
 
 
 @Injectable()
 export class ConsultUsecase {
 
-  constructor(private advertisementTable: Advertisementtable) {}
+  constructor(private advertisementTable: Advertisementtable,
+              private reportTable: ReportTable) {}
 
   async run(): Promise<ConsultResult> {
     const advertisements: Array<AdvertisementModel> = await this.advertisementTable.getAll();
-    // recoger datos de advertisement
     // recoger los grupos de advertisement
+
+    const reports: Array<ReportModel> = await this.reportTable.getAll();
     // crear ConsultResult
     return null;
 
--- src/api/releases/use_cases/releases.usecase.module.ts
+++ src/api/releases/use_cases/releases.usecase.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AdvertisementtableModule } from '../../../datasource/db/advertisementtable/advertisementtable.module';
 import { ConsultUsecase } from './consult.usecase';
+import { ReportModule } from '../../../datasource/db/reporttable/reporttable.module';
 
 @Module({
-  imports: [ AdvertisementtableModule ],
+  imports: [ AdvertisementtableModule, ReportModule ],
   controllers: [],
   providers: [ ConsultUsecase],
   exports: [ ConsultUsecase ],
