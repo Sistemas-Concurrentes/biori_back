d614c7b2db9d45b025b8ac655c239197100e2657
JaviGP2002 <javigp2002@gmail.com>
2024-04-04 11:22:34 +0200

    :sparkles: Create EventModel needed for every event consult
    LA consulta no solo tiene que traer la información básica de un evento sino también la información adyacente (número de likes, fin de inscripción si tiene y nombre del organizador).

---
+++ src/datasource/db/eventtable/model/event.model.ts
@@ -0,0 +1,59 @@
+import { IsBoolean, IsDate, IsNotEmpty } from 'class-validator';
+
+export class EventModel {
+  @IsNotEmpty()
+  id: number;
+
+  @IsNotEmpty()
+  title: string;
+
+  @IsNotEmpty()
+  category: string;
+
+  @IsNotEmpty()
+  description: string;
+
+  @IsNotEmpty()
+  organiser: number;
+
+  @IsNotEmpty()
+  organiserName: string;
+
+  @IsNotEmpty()
+  dateFrom: Date;
+
+  @IsNotEmpty()
+  location: string;
+
+  @IsNotEmpty()
+  likes: number;
+
+  @IsDate()
+  createdAt: Date;
+
+  @IsDate()
+  updatedAt: Date;
+
+  @IsBoolean()
+  isDeleted: boolean;
+
+  dateEndInscription: Date ;
+
+
+  constructor(json: any) {
+    this.id = json.id;
+    this.title = json.title;
+    this.category = json.category;
+    this.description = json.description;
+    this.organiser = json.organiser;
+    this.organiserName = json.organiser_name + ' ' + json.organiser_surname;
+    this.dateFrom = json.dateFrom;
+    this.location = json.location;
+    this.likes = json.likes_count;
+    this.createdAt = json.created;
+    this.updatedAt = json.last_update;
+    this.isDeleted = json.is_deleted;
+
+    this.dateEndInscription = json.date_end_inscription;
+  }
+}
