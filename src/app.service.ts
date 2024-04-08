830bf8ca529d92bd8530c21e8d8a402786479468
JaviGP2002 <javigp2002@gmail.com>
2024-04-08 19:00:07 +0200

    :bug: Delete unusued function on AppService

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,22 +1,10 @@
 import { Injectable } from "@nestjs/common";
-import { DbConnection } from "./datasource/db/db.connection";
 
 @Injectable()
 export class AppService {
-  constructor(private dbConnection: DbConnection) {}
+  constructor() {}
 
   getHello(): string {
     return 'Hello World!';
-  }
-  async getUsers() {
-    let conn;
-    try {
-      conn = await this.dbConnection.createConnection();
-      return await conn.query('SELECT * FROM users');
-    } catch (err) {
-      throw err;
-    } finally {
-      if (conn) conn.end();
-    }
   }
 }
