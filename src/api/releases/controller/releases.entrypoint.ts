843e42447750f009cfb099693980315f58050f1e
JaviGP2002 <javigp2002@gmail.com>
2024-04-29 18:12:47 +0200

    :boom: Add Event with guards, closes #9
    SE activan los guards por seguridad y para coger el id del usuario así como recibir el body necesario para el evento

--- src/api/releases/controller/releases.entrypoint.ts
+++ src/api/releases/controller/releases.entrypoint.ts
@@ -1,16 +1,37 @@
-import { Controller, HttpCode, HttpStatus, UseGuards, Get } from '@nestjs/common';
+import {
+  Controller,
+  HttpCode,
+  HttpStatus,
+  UseGuards,
+  Get,
+  Body,
+  Post,
+  Request,
+} from '@nestjs/common';
 import { AuthGuard } from '../../auth/guards/auth.guard';
 import { ConsultUsecase } from '../use_cases/consult.usecase';
+import {AddEventUsecase} from '../use_cases/add_event.usecase';
+import {AddEventDto} from './dto/event.dto';
 
 @Controller('releases')
 export class ReleasesEntrypoint {
-  constructor (private consultUseCase: ConsultUsecase) {}
+  constructor(
+      private consultUseCase: ConsultUsecase,
+      private addEventUseCase: AddEventUsecase) {
+  }
 
   @UseGuards(AuthGuard)
   @HttpCode(HttpStatus.OK)
   @Get('getAll')
   async getAll (){
     return this.consultUseCase.run();
+  }
+
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Post('addEvent')
+  async addEvent(@Body() addEventDto: AddEventDto, @Request() request: any) {
+    return this.addEventUseCase.run(addEventDto, request.id);
   }
 
 }
